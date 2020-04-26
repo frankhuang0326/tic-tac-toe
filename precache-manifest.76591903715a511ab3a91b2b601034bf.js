958ea8022961ae7f2d0ec0a404e668c9b4b51eae
Frank Huang <frankhuang0326@gmail.com>
2020-04-26 23:15:36 +0800

    Updates

---
+++ precache-manifest.76591903715a511ab3a91b2b601034bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95b35bcdfa7e92bc27677ab58884f4de",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "482fc5631e706edd342b",
+    "url": "/tic-tac-toe/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "139a2ebfacda02673cea",
+    "url": "/tic-tac-toe/static/js/2.3447cfc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.3447cfc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "482fc5631e706edd342b",
+    "url": "/tic-tac-toe/static/js/main.ad61abe4.chunk.js"
+  },
+  {
+    "revision": "584ea65a4be99bc11608",
+    "url": "/tic-tac-toe/static/js/runtime-main.29647a31.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tic-tac-toe/static/media/logo.5d5d9eef.svg"
+  }
+]);
